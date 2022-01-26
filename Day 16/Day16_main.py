9bfe473622bc97334d9af85398a7341b159c02ef
Machzie <96525391+Machzie@users.noreply.github.com>
2022-01-26 19:29:07 +0000

    Add files via upload

--- Day 16/Day16_main.py
+++ Day 16/Day16_main.py
@@ -11,17 +11,17 @@ user_drink = Menu().find_drink(user_choice)
 if user_choice.lower() == 'report':
     print(CoffeeMaker().report())
     print(MoneyMachine().report())
-else:
+elif Menu().find_drink(user_choice):
     print(f"You chose: {user_choice}")
     print(f"Please pay {MoneyMachine().CURRENCY}{user_drink.cost:.2f}")
+    
+    # Check resources sufficient?
+    if CoffeeMaker().is_resource_sufficient(user_drink):
 
-# Check resources sufficient?
-if CoffeeMaker().is_resource_sufficient(user_drink):
+        # Process coins
+        # Check transaction successful?
+        if MoneyMachine().make_payment(user_drink.cost):
 
-    # Process coins
-    # Check transaction successful?
-    if MoneyMachine().make_payment(user_drink.cost):
-
-        # Make coffee
-        print("Making coffee...")
-        CoffeeMaker().make_coffee(user_drink)
+            # Make coffee
+            print("Making coffee...")
+            CoffeeMaker().make_coffee(user_drink)
