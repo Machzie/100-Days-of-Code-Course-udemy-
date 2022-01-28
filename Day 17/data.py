d0c427ba31fc0c2b04bfdd97aaa6c6b013e74f1e
Machzie <96525391+Machzie@users.noreply.github.com>
2022-01-28 15:48:47 +0000

    Create data.py

---
+++ Day 17/data.py
@@ -0,0 +1,71 @@
+# Can get question data API from https://opentdb.com/
+
+question_data = [
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "'Typewriter'; is the longest word that can be typed using only the first row on a QWERTY keyboard.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "Coca-Cola's original colour was green.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "General Knowledge",
+     "type": "boolean", "difficulty": "medium",
+     "question": "The US emergency hotline is 911 because of the September 11th terrorist attacks.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "The vapor produced by e-cigarettes is actually water.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "The sum of all the numbers on a roulette wheel is 666.",
+     "correct_answer": "True", "incorrect_answers": ["False"]
+     },
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "Furby was released in 1998.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "There are 86400 seconds in a day.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "The word &quot;news&quot; originates from the first letters of the 4 main directions on a compass (North, East, West, South).",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "An eggplant is a vegetable.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "medium",
+     "question": "The bikini is named after the 'Bikini Atoll', an island where the United States conducted tests on atomic bombs.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     }
+    ]
