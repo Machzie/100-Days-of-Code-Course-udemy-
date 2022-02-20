072c8d529f16bb1b562d8c6cc1b19632be0f1963
Machzie <96525391+Machzie@users.noreply.github.com>
2022-02-20 18:01:24 +0000

    Update Day26_main.py

--- Day 26/Day26_main.py
+++ Day 26/Day26_main.py
@@ -8,9 +8,6 @@ alphabet_dict = {row.letter:row.code for (index, row) in alphabet_data.iterrows(
 
 user_word = input("Please type a word to convert: ").upper()
 
-NATO_list = []
-for letter in user_word:
-    if letter in letters:
-        NATO_list.append(alphabet_dict[letter])
+NATO_list = [alphabet_dict[letter] for letter in user_word if letter in letters]
 
 print(NATO_list)
